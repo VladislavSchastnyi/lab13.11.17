2b32430916dd7f1daff3d08f4d0d685f6e4ae591
VladislavSchastnyi <32384821+VladislavSchastnyi@users.noreply.github.com>
2017-11-13 13:42:37 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int i, n, x;
+  cout << "Vvedite razmer nosledovatel'nosti" <<  endl;
+  cin >> n;
+  if (n <= 0) {
+        return 0;
+  int a[n];
+  cout << "Vvedite elementi  nosledovatel'nosti" <<  endl;
+  for (i = 0; i < n; i++) {
+        cin >> a[i];
+  }
+  cout << "" <<  endl;
+  cin >> x;
+  int b[x - 1], c[n - x - 1];
+  for (i = 0; i  < x ; i++) {
+        b[i] = a[i];
+  }
+  for (i = 0; i  < n - x ; i++) {
+        c[i] = a[i + x];
+  }
+  for (i = 0; i  < n - x ; i++) {
+       cout <<  c[i] << "";
+  }
+  for (i = 0; i  < x ; i++) {
+        cout << b[i] << "";
+  }
+return 0;
+}
+}
+

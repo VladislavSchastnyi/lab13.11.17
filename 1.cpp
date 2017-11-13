a9d5481ea3570bea47efd07fb21e28e4bfdf0066
VladislavSchastnyi <32384821+VladislavSchastnyi@users.noreply.github.com>
2017-11-13 14:27:10 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main(){
   int n, m;
-  cout << "Vvedite element massiva" <<  endl;
+  cout << "Vvedite razmer massiva" <<  endl;
   cin >> n;
   if (n <= 0) {
         cout << "error" << endl;
+        return 1;
   }
-  int a[n];
+  cout << "" << endl;
+  int *a = new int [n];
   for (m = 0; m < n; m++) {
         cin >> a[m];
   }
-  for (m = 0; m <  n / 2; m++) {
+  for (m = 0; m <  n / 2 ; m++) {
         swap(a[m], a[n - m -1]);
   }
+
   for (m = 0; m < n; m++) {
-        if (n != m) {
-          cout << "error" << endl;
-          return 0;
-         }
-        cout << a[m] << endl;
+       cout << a[m]  << endl;
   }
+delete [] a;
 }
 
